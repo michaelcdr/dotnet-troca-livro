0534d26ab0bb915f73557c868651a731023d2b64
Michael Costa dos Reis <michael@gia.com.br>
2024-10-08 22:19:05 -0300

    add some refactorings.

--- src/TrocaLivro.Aplicacao/CasosDeUsos/Comandos/Usuario/RegistrarUsuario/RegistrarUsuarioModel.cs
+++ src/TrocaLivro.Aplicacao/CasosDeUsos/Comandos/Usuario/RegistrarUsuario/RegistrarUsuarioModel.cs
@@ -1,31 +1,30 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace TrocaLivro.Aplicacao.CasosDeUsos
+namespace TrocaLivro.Aplicacao.CasosDeUsos;
+
+public class RegistrarUsuarioModel
 {
-    public class RegistrarUsuarioModel
-    {
-        [Required(ErrorMessage = "Informe o {0}")]
-        [Display(Name = "Usuário")]
-        public string Usuario { get; set; }
-        
-        [Required(ErrorMessage = "Informe o {0}")]
-        public string Nome { get; set; }
+    [Required(ErrorMessage = "Informe o {0}")]
+    [Display(Name = "Usuário")]
+    public string Usuario { get; set; }
+    
+    [Required(ErrorMessage = "Informe o {0}")]
+    public string Nome { get; set; }
 
-        [Required(ErrorMessage ="Informe o {0}")]
-        [StringLength(100, ErrorMessage = "Máximo 100 caracteres")]
-        public string Sobrenome { get; set; }
+    [Required(ErrorMessage ="Informe o {0}")]
+    [StringLength(100, ErrorMessage = "Máximo 100 caracteres")]
+    public string Sobrenome { get; set; }
 
-        [Required(ErrorMessage = "Informe o {0}")]
-        [EmailAddress(ErrorMessage = "E-mail inválido")]
-        [Display(Name = "E-mail")]
-        public string Email { get; set; }
+    [Required(ErrorMessage = "Informe o {0}")]
+    [EmailAddress(ErrorMessage = "E-mail inválido")]
+    [Display(Name = "E-mail")]
+    public string Email { get; set; }
 
-        [Required(ErrorMessage = "Informe a {0}")]
-        public string Senha { get; set; }
+    [Required(ErrorMessage = "Informe a {0}")]
+    public string Senha { get; set; }
 
-        [Required(ErrorMessage = "Confirme a {0}")]
-        [Display(Name = "Confirmar senha")]
-        [Compare("Senha",ErrorMessage = "A confirmação deve ser igual a senha.")]
-        public string ConfirmarSenha { get; set; }
-    }
+    [Required(ErrorMessage = "Confirme a {0}")]
+    [Display(Name = "Confirmar senha")]
+    [Compare("Senha",ErrorMessage = "A confirmação deve ser igual a senha.")]
+    public string ConfirmarSenha { get; set; }
 }
--- src/WebApp/Controllers/UsuarioController.cs
+++ src/WebApp/Controllers/UsuarioController.cs
@@ -14,125 +14,124 @@ using TrocaLivro.Aplicacao.ViewModels;
 using TrocaLivro.Dominio.Responses;
 using WebApp.Filtros;
 
-namespace WebApp.Controllers
+namespace WebApp.Controllers;
+
+public class UsuarioController : BaseController
 {
-    public class UsuarioController : BaseController
+    private readonly UsuarioApiClient _usuarioApi;
+    private readonly IMapper _mapper;
+
+    public UsuarioController(UsuarioApiClient contaApiClient, IMapper mapper)
     {
-        private readonly UsuarioApiClient _usuarioApi;
-        private readonly IMapper _mapper;
+        this._usuarioApi = contaApiClient;
+        this._mapper = mapper;
+    }
 
-        public UsuarioController(UsuarioApiClient contaApiClient, IMapper mapper)
+    public IActionResult Login()
+    {
+        if (User.Identity.IsAuthenticated) return RedirectToAction("Index", "Home");
+        
+        return View();
+    }
+
+    public IActionResult _Logar() => View(new LogarUsuarioModel()); 
+
+    [HttpPost, ModelStateValidador]
+    public async Task<JsonResult> Logar(LogarUsuarioModel model)
+    {
+        if (!ModelState.IsValid) return Json(new { Sucesso = false, Mensagem = "Informe os campos usuário e senha" });
+
+        AppResponse<LogarUsuarioResultado> resultado = await _usuarioApi.Logar(model);
+
+        if (!resultado.Sucesso)
         {
-            this._usuarioApi = contaApiClient;
-            this._mapper = mapper;
+            string mensagem = resultado.Erros != null 
+                ?"<br />" + string.Join("<br/>", resultado.Erros.Select(e => e.Mensagem).ToArray())
+                : resultado.Mensagem;
+
+            return Json(new { Sucesso = false, Mensagem = mensagem });
         }
 
-        public IActionResult Login()
+        await AutenticarRegistrandoClaimns(model.Usuario, resultado);
+
+        return Json(new { sucesso = true, urlDestino = Url.Action("Index", "Home") });            
+    }
+
+    private async Task AutenticarRegistrandoClaimns(string username, 
+                                                    AppResponse<LogarUsuarioResultado> logarUsuarioResultado)
+    {
+        var claims = new List<Claim>
         {
-            if (User.Identity.IsAuthenticated)
-                return RedirectToAction("Index", "Home");
-            
-            return View();
-        }
+            new Claim("UsuarioId", logarUsuarioResultado.Dados.UsuarioId),
+            new Claim(ClaimTypes.Name, username),
+            new Claim(ClaimTypes.NameIdentifier, username),
+            new Claim("Token", logarUsuarioResultado.Dados.Token),
+            new Claim(ClaimTypes.Role,logarUsuarioResultado.Dados.Role)
+        };
 
-        public IActionResult _Logar() => View(new LogarUsuarioModel()); 
+        var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+        var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
 
-        [HttpPost, ModelStateValidador]
-        public async Task<JsonResult> Logar(LogarUsuarioModel model)
+        var authProp = new AuthenticationProperties
         {
-            if (!ModelState.IsValid) return Json(new { Sucesso = false, Mensagem = "Informe os campos usuário e senha" });
+            IssuedUtc = DateTime.UtcNow,
+            ExpiresUtc = DateTimeOffset.UtcNow.AddDays(2), 
+            IsPersistent = true
+        };
+        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal, authProp);
+    }
 
-            AppResponse<LogarUsuarioResultado> resultado = await _usuarioApi.Logar(model);
+    public IActionResult Registrar() => View(new RegistrarUsuarioModel());
 
-            if (!resultado.Sucesso)
-            {
-                string mensagem = resultado.Erros != null 
-                    ?"<br />" + string.Join("<br/>", resultado.Erros.Select(e => e.Mensagem).ToArray())
-                    : resultado.Mensagem;
+    [HttpPost]
+    public async Task<ActionResult> Registrar(RegistrarUsuarioModel model)
+    {
+        if (!ModelState.IsValid) return View(model);
 
-                return Json(new { Sucesso = false, Mensagem = mensagem });
-            }
+        AppResponse<RegistrarUsuarioResultado> resultado = await _usuarioApi.Registrar(model);
 
-            await AutenticarRegistrandoClaimns(model.Usuario, resultado);
-
-            return Json(new { sucesso = true, urlDestino = Url.Action("Index", "Home") });            
-        }
-
-        private async Task AutenticarRegistrandoClaimns(string username, AppResponse<LogarUsuarioResultado> logarUsuarioResultado)
+        if (!resultado.Sucesso)
         {
-            var claims = new List<Claim>
-            {
-                new Claim("UsuarioId", logarUsuarioResultado.Dados.UsuarioId),
-                new Claim(ClaimTypes.Name, username),
-                new Claim(ClaimTypes.NameIdentifier, username),
-                new Claim("Token", logarUsuarioResultado.Dados.Token),
-                new Claim(ClaimTypes.Role,logarUsuarioResultado.Dados.Role)
-            };
+            foreach (var item in resultado.Erros)
+                ModelState.AddModelError("", item.Mensagem);
 
-            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
-
-            var authProp = new AuthenticationProperties
-            {
-                IssuedUtc = DateTime.UtcNow,
-                ExpiresUtc = DateTimeOffset.UtcNow.AddDays(2), 
-                IsPersistent = true
-            };
-            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal, authProp);
+            return View(model);
         }
+        
+        AppResponse<LogarUsuarioResultado> logarUsuarioResultado = await _usuarioApi.Logar(new LogarUsuarioModel(model.Usuario , model.Senha));
 
-        public IActionResult Registrar() => View(new RegistrarUsuarioModel());
+        await AutenticarRegistrandoClaimns(model.Usuario, logarUsuarioResultado);
 
-        [HttpPost]
-        public async Task<ActionResult> Registrar(RegistrarUsuarioModel model)
-        {
-            if (!ModelState.IsValid) return View(model);
+        return RedirectToAction("Index", "Home");
+    }
 
-            AppResponse<RegistrarUsuarioResultado> resultado = await _usuarioApi.Registrar(model);
+    public IActionResult AprovarTrocas() => View();
 
-            if (!resultado.Sucesso)
-            {
-                foreach (var item in resultado.Erros)
-                    ModelState.AddModelError("", item.Mensagem);
+    public async Task<IActionResult> Sair()
+    {
+        await HttpContext.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
 
-                return View(model);
-            }
-            
-            AppResponse<LogarUsuarioResultado> logarUsuarioResultado = await _usuarioApi.Logar(new LogarUsuarioModel(model.Usuario , model.Senha));
+    [AuthorizeCustomizado]
+    public IActionResult MinhaConta()
+    {
+        return View(new MinhaContaViewModel());
+    }
 
-            await AutenticarRegistrandoClaimns(model.Usuario, logarUsuarioResultado);
+    [AuthorizeCustomizado]
+    public async Task<IActionResult> _Editar()
+    {
+        base.AtualizarToken(this._usuarioApi);
+        AppResponse<ObterUsuarioResultado> resultado = await _usuarioApi.Obter(User.Identity.Name);
+        var model = _mapper.Map<EditarUsuarioModel>(resultado.Dados);
+        return PartialView(model);
+    }
 
-            return RedirectToAction("Index", "Home");
-        }
-
-        public IActionResult AprovarTrocas() => View();
-
-        public async Task<IActionResult> Sair()
-        {
-            await HttpContext.SignOutAsync();
-            return RedirectToAction("Index", "Home");
-        }
-
-        [AuthorizeCustomizado]
-        public IActionResult MinhaConta()
-        {
-            return View(new MinhaContaViewModel());
-        }
-
-        [AuthorizeCustomizado]
-        public async Task<IActionResult> _Editar()
-        {
-            base.AtualizarToken(this._usuarioApi);
-            AppResponse<ObterUsuarioResultado> resultado = await _usuarioApi.Obter(User.Identity.Name);
-            var model = _mapper.Map<EditarUsuarioModel>(resultado.Dados);
-            return PartialView(model);
-        }
-
-        [HttpPost, ModelStateValidador, AuthorizeCustomizado]
-        public async Task<JsonResult> _Editar(EditarUsuarioCommand model)
-        {
-            base.AtualizarToken(this._usuarioApi);
-            return Json(await _usuarioApi.Atualizar(model));
-        }
+    [HttpPost, ModelStateValidador, AuthorizeCustomizado]
+    public async Task<JsonResult> _Editar(EditarUsuarioCommand model)
+    {
+        base.AtualizarToken(this._usuarioApi);
+        return Json(await _usuarioApi.Atualizar(model));
     }
 }
